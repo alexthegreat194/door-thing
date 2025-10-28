81dd3945d22586de06e64e66532e0a9c5ac6ee09
Alex Harlan <alexharlan194@gmail.com>
2025-10-27 19:26:16 -0700

    web interface

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 from flask import Flask, render_template
+from enum import Enum
+
+from flask.globals import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+class Status(Enum):
+    Idle=0
+    Busy=1
+    Meeting=2
+
+currentStatus = Status.Idle
+
+@app.get('/api/status')
+def status():
+    return {
+        "status": int(currentStatus.value)
+    }
+
+@app.post('/api/status')
+def statusPost():
+    global currentStatus
+    foundStatus = request.form["status"]
+    if not foundStatus:
+        print('no found status')
+        return {
+            "status": int(currentStatus.value)
+        }
+
+    statusFormatted = int(foundStatus)
+    print(statusFormatted, type(statusFormatted))
+
+    if statusFormatted not in [0, 1, 2]:
+        print('bad formatting - invalid status value')
+        return {
+            "status": int(currentStatus.value)
+        }
+
+    currentStatus = Status(statusFormatted)
+
+    return {
+        "status": int(currentStatus.value)
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
